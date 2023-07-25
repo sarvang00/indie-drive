d039c211356d7e3c08c26d4b02ab82861927a889
Sarvang Dave <sarvang00@gmail.com>
2023-07-24 21:33:24 -0300

    Modiufying Admin section

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import File
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_public', 'upload_date')
+    list_display_links = ('id', 'name')
+    # list_filter = ('upload_date') change to user
+    search_fields = ('name') # also add user
+    list_per_page = 25
+
+admin.site.register(File)
