0030724116a244be3b71107f2fa7c6157956e28e
Sarvang Dave <sarvang00@gmail.com>
2023-07-24 21:34:12 -0300

    Adding database relation

--- indiedrive/settings.py
+++ indiedrive/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'indiedrive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'indiedrive',
+        'USER': 'root',
+        'PASSWORD': 'MayPass@0523',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
@@ -128,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
